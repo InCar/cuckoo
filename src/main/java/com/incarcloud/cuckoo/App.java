b075389b1dc2a8f3a1967ecd1cf98ee8d3fe045d
XGH <ghxiong@incarcloud.com>
2023-06-06 18:14:34 +0800

    module-info.java

--- src/main/java/com/incarcloud/cuckoo/App.java
+++ src/main/java/com/incarcloud/cuckoo/App.java
@@ -3,6 +3,7 @@ package com.incarcloud.cuckoo;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
@@ -12,13 +13,30 @@ public class App implements CommandLineRunner {
         org.springframework.boot.SpringApplication.run(App.class, args);
     }
 
+    private Environment env;
+
+    public App(Environment env) {
+        this.env = env;
+    }
+
     @Override
     public void run(String... args) throws Exception {
         GitVer version = new GitVer();
         s_logger.info("AppVer: {}", version.getVersion());
+        loggingBasicInfo();
 
         Runtime.getRuntime().addShutdownHook(new Thread(()->{
             s_logger.info("Quit.");
         }));
+    }
+
+    private void loggingBasicInfo() {
+        String address = env.getProperty("server.address");
+        String port = env.getProperty("server.port");
+
+        if(port != null) {
+            s_logger.info("WebServer is listening on \033[0;34mhttp://{}\033[0m\033[0;96m:{}\033[0m",
+                    address, port);
+        }
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.incarcloud.cuckoo {
+    requires org.slf4j;
+    requires spring.boot;
+}
